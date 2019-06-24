96ec6112421b062bf691bc4c20fa1ec606ac95dc
Ethan Daniel <ethandaniel@berkeley.edu>
2019-06-24 13:19:23 -0700

    Initial Commit

---
+++ Workout Log/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workout Log
+//
+//  Created by Ethan Daniel on 6/24/19.
+//  Copyright © 2019 Ethan Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
